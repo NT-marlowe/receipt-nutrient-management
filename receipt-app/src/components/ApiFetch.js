62cfcd7bd23614eff255abac2418d25093b1a49a
NT-marlowe <n.tezuka196.ninj@gmail.com>
2022-03-08 10:04:41 +0900

    feat: get and post API(incomplete)

--- receipt-app/src/components/ApiFetch.js
+++ receipt-app/src/components/ApiFetch.js
@@ -1,17 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 const ApiFetch = () => {
-    
     const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
+    // this function is to-be-implemented!!
+    function getNutritionInfo() {
+        // setPosts(res.data);
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(res => {
             setPosts(res.data)
-        })
-    }, [])
+        });
+    }
     
+    function postImages() {
+        // axios.post('https://anysweb.co.jp/api/')
+        // .then(res => console.log(res))
+        console.log("Image has been posted");
+    }
 
+    // useEffect(() => {
+    useEffect(() => {
+        getNutritionInfo();
+        postImages();
+        }
+        , []);
+    
+        
+    // useEffect(() => {
+    //     axios.get("http://localhost:8000/api/todo").then((res) => {
+    //     setTodoList(res.data);
+    //     });
+    // }, [])
+    //     -    
+    
     return (
         <div>
             <ul>
