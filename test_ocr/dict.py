d77842cf5c5aa18362a11c08b33814312fd6cafc
riku-81 <riku20020801@gmail.com>
2022-03-10 14:17:08 +0900

    first commit

---
+++ test_ocr/dict.py
@@ -0,0 +1,6 @@
+"""
+nutrition = [タンパク質, 脂質, 炭水化物, 食物繊維, Na, K, Ca, Mg, P, Fe, Zn, Cu, Mn, I, Se, Cr, モリブテン, ビタミンA, D, E, B1, B2, ナイアシン, B6, B12, 葉酸, パントテン酸, ビオチン, C, ]
+"""
+nutrition_facts = {
+    
+}
---
+++ test_ocr/main.py
@@ -0,0 +1,30 @@
+from http import client
+import io
+import os
+from urllib import response
+
+from google.cloud import vision
+
+client = vision.ImageAnnotatorClient()
+
+file_name = os.path.abspath('./sample.png')
+
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.Image(content=content)
+
+response = client.document_text_detection(
+    image=image,
+)
+
+output_text = ''
+for page in response.full_text_annotation.pages:
+    for block in page.blocks:
+        for paragraph in block.paragraphs:
+            for word in paragraph.words:
+                output_text += ''.join([
+                    symbol.text for symbol in word.symbols
+                ])
+
+print(output_text)
