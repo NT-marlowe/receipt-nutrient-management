48272ca51a5aa53b6d0550759bd688d540d8fae6
NT-marlowe <n.tezuka196.ninj@gmail.com>
2022-03-22 07:53:28 +0900

    feat: sort histories with date

--- receipt-app/src/pages/ReceiptHistoryPage.js
+++ receipt-app/src/pages/ReceiptHistoryPage.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ReceiptHistoryList from "../components/receipts/ReceiptHistoryList";
 
+const dateSort = (a, b) => {
+  if (a.date < b.date) return 1;
+  else if (a.date > b.date) return -1;
+  return 0;
+};
+
 const ReceiptHistoryPage = () => {
   const [histories, setHistories] = useState([]);
   useEffect(() => {
     axios
       .get("http://127.0.0.1:8000/history/")
       .then((response) => {
-        setHistories(() => response.data.reverse());
+        setHistories(() => response.data.sort(dateSort));
       })
       .catch((error) => {
         console.error(error);
